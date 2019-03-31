3c24025d5b349d42a7a11afe24ca441b50adba59
Ross King <sleeping.stars@gmail.com>
2019-03-31 19:13:55 +0100

    Added main tag, added Javascript

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+(function() {
+  const els = {
+    body: document.querySelector('body'),
+    drawer: document.querySelector('[data-drawer]'),
+    drawerTrigger: document.querySelector('[data-drawer-trigger]'),
+    drawerClose: document.querySelector('[data-drawer-close]'),
+    drawersOverlay: document.querySelector('[data-drawers-overlay]')
+  }
+
+  // Set up drawer trigger button
+  els.drawerTrigger.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.body.classList.add('utl-DrawerActive')
+  })
+
+  // Set up drawer close button
+  els.drawerTrigger.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    els.body.classList.remove('utl-DrawerActive')
+  })
+})()
