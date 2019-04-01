8df1696bf8d7a2e8a330159217e08d89e17311f6
Ross King <sleeping.stars@gmail.com>
2019-04-01 15:37:22 +0100

    Clicking nav links now scrolls to element minus header height, added functionality for hero button scroll down, changed header padding on mobile - shrank title on mobile

--- dist/js/app.bundle.js
+++ dist/js/app.bundle.js
@@ -8,7 +8,9 @@
   function Init() {
     document.addEventListener('DOMContentLoaded', function () {
       Drawers();
+      HeroScrollDown();
       ProjectsCarousel();
+      ScrollToLink();
     });
   }
 
@@ -18,7 +20,8 @@
       drawer: document.querySelector('[data-drawer]'),
       drawerTrigger: document.querySelector('[data-drawer-trigger]'),
       drawerClose: document.querySelector('[data-drawer-close]'),
-      drawersOverlay: document.querySelector('[data-drawers-overlay]')
+      drawersOverlay: document.querySelector('[data-drawers-overlay]'),
+      drawerLinks: [].slice.call(document.querySelectorAll('[data-drawer] nav a'))
 
       // Set up drawer trigger button
     };els.drawerTrigger.addEventListener('click', function (e) {
@@ -40,6 +43,30 @@
 
       els.body.classList.remove('utl-DrawerActive');
     });
+
+    // Close drawer on drawer link click
+    els.drawerLinks.forEach(function (link) {
+      link.addEventListener('click', function () {
+        els.body.classList.remove('utl-DrawerActive');
+      });
+    });
+  }
+
+  function HeroScrollDown() {
+    var els = {
+      header: document.querySelector('.hd-Header'),
+      heroButton: document.querySelector('[data-hero-button]')
+    };
+
+    els.heroButton.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      var windowHeight = window.innerHeight;
+      var headerHeight = els.header.offsetHeight;
+      var heroScrollOffset = windowHeight - headerHeight;
+
+      ScrollTo(heroScrollOffset);
+    });
   }
 
   function ProjectsCarousel() {
@@ -49,6 +76,33 @@
       contain: true,
       prevNextButtons: false,
       watchCSS: true
+    });
+  }
+
+  function ScrollToLink() {
+    var els = {
+      header: document.querySelector('.hd-Header'),
+      links: [].slice.call(document.querySelectorAll('a[href^="#"]'))
+    };
+
+    els.links.forEach(function (link) {
+      link.addEventListener('click', function (e) {
+        var headerHeight = els.header.offsetHeight;
+        var linkHref = link.getAttribute("href").slice(1);
+        var linkEl = document.getElementById(linkHref);
+        var linkElOffsetTop = linkEl.offsetTop - headerHeight;
+
+        e.preventDefault();
+        ScrollTo(linkElOffsetTop);
+      });
+    });
+  }
+
+  function ScrollTo(offsetTop) {
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: offsetTop
     });
   }
 })();
--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,9 @@
   function Init() {
     document.addEventListener('DOMContentLoaded', () => {
       Drawers()
+      HeroScrollDown()
       ProjectsCarousel()
+      ScrollToLink()
     })
   }
 
@@ -16,7 +18,8 @@
       drawer: document.querySelector('[data-drawer]'),
       drawerTrigger: document.querySelector('[data-drawer-trigger]'),
       drawerClose: document.querySelector('[data-drawer-close]'),
-      drawersOverlay: document.querySelector('[data-drawers-overlay]')
+      drawersOverlay: document.querySelector('[data-drawers-overlay]'),
+      drawerLinks: [].slice.call(document.querySelectorAll('[data-drawer] nav a'))
     }
 
     // Set up drawer trigger button
@@ -39,6 +42,30 @@
 
       els.body.classList.remove('utl-DrawerActive')
     })
+
+    // Close drawer on drawer link click
+    els.drawerLinks.forEach(function(link){
+      link.addEventListener('click', function() {
+        els.body.classList.remove('utl-DrawerActive')
+      })
+    })
+  }
+
+  function HeroScrollDown() {
+    const els = {
+      header: document.querySelector('.hd-Header'),
+      heroButton: document.querySelector('[data-hero-button]')
+    }
+
+    els.heroButton.addEventListener('click', function(e) {
+      e.preventDefault()
+
+      const windowHeight = window.innerHeight
+      const headerHeight = els.header.offsetHeight
+      const heroScrollOffset = windowHeight - headerHeight
+
+      ScrollTo(heroScrollOffset)
+    })
   }
 
   function ProjectsCarousel() {
@@ -49,5 +76,32 @@
       prevNextButtons: false,
       watchCSS: true
     })
+  }
+
+  function ScrollToLink() {
+    const els = {
+      header: document.querySelector('.hd-Header'),
+      links: [].slice.call(document.querySelectorAll('a[href^="#"]'))
+    }
+
+    els.links.forEach(function(link) {
+      link.addEventListener('click', function(e) {
+        const headerHeight = els.header.offsetHeight
+        const linkHref = link.getAttribute("href").slice(1)
+        const linkEl = document.getElementById(linkHref)
+        const linkElOffsetTop = linkEl.offsetTop - headerHeight
+
+        e.preventDefault()
+        ScrollTo(linkElOffsetTop)
+      })
+    })
+  }
+
+  function ScrollTo(offsetTop) {
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: offsetTop
+    });
   }
 })()
