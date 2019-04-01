9c065dcd29937ebc08894727a6473f33dfc09d17
Ross King <sleeping.stars@gmail.com>
2019-04-01 10:40:54 +0100

    Added flickity, made portfolio items a carousel on tablet down, shrank size of section lead text and service cards titles with two new font mixins, added functionality for push menu on screen sizes below desktop.

--- dist/js/app.bundle.js
+++ dist/js/app.bundle.js
@@ -1,24 +1,54 @@
 'use strict';
 
 (function () {
-  var els = {
-    body: document.querySelector('body'),
-    drawer: document.querySelector('[data-drawer]'),
-    drawerTrigger: document.querySelector('[data-drawer-trigger]'),
-    drawerClose: document.querySelector('[data-drawer-close]'),
-    drawersOverlay: document.querySelector('[data-drawers-overlay]')
+  // var Flickity = require('flickity');
 
-    // Set up drawer trigger button
-  };els.drawerTrigger.addEventListener('click', function (e) {
-    e.preventDefault();
+  Init();
 
-    document.body.classList.add('utl-DrawerActive');
-  });
+  function Init() {
+    document.addEventListener('DOMContentLoaded', function () {
+      Drawers();
+      ProjectsCarousel();
+    });
+  }
 
-  // Set up drawer close button
-  els.drawerTrigger.addEventListener('click', function (e) {
-    e.preventDefault();
+  function Drawers() {
+    var els = {
+      body: document.body,
+      drawer: document.querySelector('[data-drawer]'),
+      drawerTrigger: document.querySelector('[data-drawer-trigger]'),
+      drawerClose: document.querySelector('[data-drawer-close]'),
+      drawersOverlay: document.querySelector('[data-drawers-overlay]')
 
-    els.body.classList.remove('utl-DrawerActive');
-  });
+      // Set up drawer trigger button
+    };els.drawerTrigger.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      els.body.classList.add('utl-DrawerActive');
+    });
+
+    // Set up drawer close button
+    els.drawerClose.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      els.body.classList.remove('utl-DrawerActive');
+    });
+
+    // Close drawer on overlay click
+    els.drawersOverlay.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      els.body.classList.remove('utl-DrawerActive');
+    });
+  }
+
+  function ProjectsCarousel() {
+    var flkty = new Flickity('.prt-Portfolio_Items', {
+      cellAlign: 'left',
+      cellSelector: '.prt-Portfolio_Item',
+      contain: true,
+      prevNextButtons: false,
+      watchCSS: true
+    });
+  }
 })();
--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,53 @@
 (function() {
-  const els = {
-    body: document.querySelector('body'),
-    drawer: document.querySelector('[data-drawer]'),
-    drawerTrigger: document.querySelector('[data-drawer-trigger]'),
-    drawerClose: document.querySelector('[data-drawer-close]'),
-    drawersOverlay: document.querySelector('[data-drawers-overlay]')
+  // var Flickity = require('flickity');
+
+  Init()
+
+  function Init() {
+    document.addEventListener('DOMContentLoaded', () => {
+      Drawers()
+      ProjectsCarousel()
+    })
   }
 
-  // Set up drawer trigger button
-  els.drawerTrigger.addEventListener('click', function(e) {
-    e.preventDefault();
+  function Drawers() {
+    const els = {
+      body: document.body,
+      drawer: document.querySelector('[data-drawer]'),
+      drawerTrigger: document.querySelector('[data-drawer-trigger]'),
+      drawerClose: document.querySelector('[data-drawer-close]'),
+      drawersOverlay: document.querySelector('[data-drawers-overlay]')
+    }
 
-    document.body.classList.add('utl-DrawerActive')
-  })
+    // Set up drawer trigger button
+    els.drawerTrigger.addEventListener('click', function(e) {
+      e.preventDefault();
 
-  // Set up drawer close button
-  els.drawerTrigger.addEventListener('click', function(e) {
-    e.preventDefault();
+      els.body.classList.add('utl-DrawerActive')
+    })
 
-    els.body.classList.remove('utl-DrawerActive')
-  })
+    // Set up drawer close button
+    els.drawerClose.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      els.body.classList.remove('utl-DrawerActive')
+    })
+
+    // Close drawer on overlay click
+    els.drawersOverlay.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      els.body.classList.remove('utl-DrawerActive')
+    })
+  }
+
+  function ProjectsCarousel() {
+    var flkty = new Flickity( '.prt-Portfolio_Items', {
+      cellAlign: 'left',
+      cellSelector: '.prt-Portfolio_Item',
+      contain: true,
+      prevNextButtons: false,
+      watchCSS: true
+    })
+  }
 })()
